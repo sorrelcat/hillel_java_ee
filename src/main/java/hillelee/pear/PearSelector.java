34293f1f8c23632e4751b6ca9d8ced4cb3fc2456
sorrelcat <o.schest@gmail.com>
2017-11-07 00:04:29 +0200

    Optimize PearSelector class with lambdas. Create test for PearSelector class, but it doesn't work - error "class not found".

--- src/main/java/hillelee/pear/PearSelector.java
+++ src/main/java/hillelee/pear/PearSelector.java
@@ -1,29 +1,30 @@
 package hillelee.pear;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PearSelector {
 
-    interface PearPredicate extends Predicate<Boolean> {
-        Boolean test(Pear pear);
+    interface PearPredicate extends Predicate<Pear> {
+
+        @Override
+        boolean test(Pear pear);
     }
 
     class YellowPredicate implements PearPredicate {
 
         @Override
-        public Boolean test(Pear pear) {
+        public boolean test(Pear pear) {
             return pear.getColor().equals("YELLOW");
         }
+
     }
 
     class LightPredicate implements PearPredicate {
 
         @Override
-        public Boolean test(Pear pear) {
+        public boolean test(Pear pear) {
             return pear.getWeight() <= 50;
         }
     }
@@ -37,25 +38,21 @@ public class PearSelector {
 
     public static List<Pear> filterHeavy(List<Pear> pears, Integer weight) {
 
-        List<Pear> result = new ArrayList<>();
-        result.addAll(pears.stream().allMatch(new Predicate<Pear>() {
-            @Override
-            public boolean test(Pear pear) {
-                return new LightPredicate(pear);
-            }
-        }));
+        return new ArrayList<>(pears.stream()
+                .filter(pear -> pear.getWeight() <= weight)
+                .collect(Collectors.toList()));
     }
 
-    /*
-    public static List<Apple> filterHeavy(List<Apple> apples, Integer weight) {
-        List<Apple> result = new ArrayList<>();
-        for(Apple apple : apples) {
-            if(apple.getWeight() > weight) {
-                result.add(apple);
-            }
-        }
-        return result;
+    public static List<Pear> filterYellow(List<Pear> pears, String color) {
+        return new ArrayList<>(pears.stream()
+                .filter(pear -> pear.getColor().equals(color))
+                .collect(Collectors.toList()));
     }
-     */
+
+    public static List<Pear> filter(List<Pear> pears, PearPredicate predicate) {
+        return new ArrayList<Pear>(pears.stream()
+        .filter(predicate)
+        .collect(Collectors.toList()));
+    }
 
 }
