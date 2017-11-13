4eb5edd491767c80ba57f354ef7ad5fd1905c2da
sorrelcat <o.schest@gmail.com>
2017-11-13 11:11:32 +0200

    Added Top3HighCalories tests - stream and classic variants

--- src/test/java/hillelee/restraunt/RestarauntTest.java
+++ src/test/java/hillelee/restraunt/RestarauntTest.java
@@ -3,9 +3,12 @@ package hillelee.restraunt;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -14,14 +17,14 @@ public class RestarauntTest {
     static final Integer LOW_CALORIES = 500;
 
     List<Dish> restrauntMenu = ImmutableList.of(new Dish("CAESAR", 350, true, DishType.CHICKEN),
-            new Dish("MARGARITHA", 1500, true, DishType.VEGETABLES),
+            new Dish("MARGARITHA", 1100, true, DishType.VEGETABLES),
             new Dish("HUMUS", 500, true, DishType.VEGETABLES),
             new Dish("SHAURMA", 950, false, DishType.CHICKEN),
             new Dish("CUTLET", 350, true, DishType.BEEF),
             new Dish("FO BO", 300, false, DishType.BEEF),
             new Dish("MEATBALLS", 450, false, DishType.CHICKEN),
             new Dish("SALAD", 200, true, DishType.VEGETABLES),
-            new Dish("BARBECUE", 1200, false, DishType.BEEF),
+            new Dish("BARBECUE", 1500, false, DishType.BEEF),
             new Dish("FRENCH FRIES", 550, false, DishType.VEGETABLES)
     );
 
@@ -46,6 +49,21 @@ public class RestarauntTest {
 
     @Test
     public void classicTop3HighCalories() throws Exception {
-        // List<Dish> sortedMenu = Collections.sort(restaraunt.getMenu());
+        List<Dish> mutableMenu = new ArrayList<>();
+        mutableMenu.addAll(restaraunt.getMenu());
+
+        Collections.sort(mutableMenu, Comparator.comparing(Dish::getCalories).reversed());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(mutableMenu.get(i));
+        }
+    }
+
+    @Test
+    public void streamTop3HighCalories() throws Exception {
+        restaraunt.getMenu().stream()
+                .sorted(Comparator.comparing(Dish::getCalories).reversed())
+                .peek(System.out::println)
+                .limit(3)
+                .collect(Collectors.toList());
     }
 }
