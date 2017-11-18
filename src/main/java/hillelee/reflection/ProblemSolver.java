a8ac301851e86e4c1976fb26906283854edc4b48
sorrelcat <o.schest@gmail.com>
2017-11-18 10:44:30 +0200

    ProblemSolver with streams - solved problem with sneakythrows

--- src/main/java/hillelee/reflection/ProblemSolver.java
+++ src/main/java/hillelee/reflection/ProblemSolver.java
@@ -4,13 +4,15 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
  * Created by JavaEE on 28.10.2017.
  */
 public class ProblemSolver {
-@SneakyThrows
+
+    @SneakyThrows
     public String solve(Object problem) {
 
         /*Class<?> aClass = problem.getClass();
@@ -20,14 +22,26 @@ public class ProblemSolver {
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer annotation"));*/
 
-        return Stream.of(problem)
-                .map(Object::getClass)
-                .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
-                .filter(method -> method.isAnnotationPresent(CorrectAnswer.class))
-                .map(method -> (String) method.invoke(problem))
+        return Stream.of(problem) // stream from 1 element
+                .map(Object::getClass) // stream from 1 element - class of problem
+                .flatMap(clazz -> Arrays.stream(clazz.getMethods())) // stream from any elements - their methods, new stream on base of original stream
+                .filter(method -> method.isAnnotationPresent(CorrectAnswer.class)) // check annotation on methods
+                //.map(method -> (String) method.invoke(problem)) // method (not problem in stream, problem from method parameter) with key annotation to string
+                .map(invokeOn(problem)) // to get string from method
                 .findFirst()
-                .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer annotation"));
+                .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer annotation")); // if optional is null, exception
+    }
+
+    private Function<Method, String> invokeOn(Object object) {
+
+        return method -> {
+            try {
+                return (String) method.invoke(object);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
 
-// invoke, flatmap, reflection, map
+// invoke, flatmap, reflection, map, effectively final
